0a1527094b3b11751b76ce382616d7f0066798aa
John Haupenthal <johnhaup@gmail.com>
2021-12-24 15:06:55 -0800

    Dim player on no mouse hover

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Player from "react-h5-audio-player";
 import "react-h5-audio-player/src/styles.scss";
 
@@ -8,26 +8,38 @@ const songs = [
 ];
 
 export const AudioPlayer = () => {
+  const [hovered, setHovered] = useState(false);
+  const playerRef = useRef<Player | null>(null);
   const [songIndex, setSongIndex] = useState(0);
   const onEnded = () => {
     setSongIndex(songIndex === 0 ? 1 : 0);
   };
 
   return (
-    <Player
-      autoPlay
-      src={songs[songIndex]}
-      hasDefaultKeyBindings={false}
-      showJumpControls={false}
+    <div
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
       style={{
+        opacity: hovered ? 1 : 0.3,
+        zIndex: 999,
         position: "absolute",
         bottom: 24,
         right: 24,
-        width: "300px",
-        zIndex: 999,
       }}
-      onEnded={onEnded}
-      customAdditionalControls={[]}
-    />
+    >
+      <Player
+        ref={playerRef}
+        autoPlay={true}
+        src={songs[songIndex]}
+        hasDefaultKeyBindings={false}
+        showJumpControls={false}
+        style={{
+          width: "300px",
+          borderRadius: 8,
+        }}
+        onEnded={onEnded}
+        customAdditionalControls={[]}
+      />
+    </div>
   );
 };
