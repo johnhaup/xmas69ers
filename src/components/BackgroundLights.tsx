83beef883d6e44909596b4e63ff2bee60893c5c3
John Haupenthal <johnhaup@gmail.com>
2021-12-24 13:46:23 -0800

    Add player to App

--- src/components/BackgroundLights.tsx
+++ src/components/BackgroundLights.tsx
@@ -6,8 +6,8 @@ export const BackgroundLights = () => {
   return (
     <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
       <div className="bokeh">
-        {lights.map(() => (
-          <div className="light"></div>
+        {lights.map((_, i) => (
+          <div className="light" key={i}></div>
         ))}
       </div>
     </div>
